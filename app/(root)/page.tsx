ae9095b5aba4f4b22716d53e2f26d9cce8942e17
sonyerg <ericksonrguinto@gmail.com>
2024-05-19 17:36:16 +1200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-4xl font-bold flex justify-center items-center h-[60vh]">
+      <Button size="lg" className="uppercase">
+        Click me
+      </Button>
+    </div>
+  );
+}
