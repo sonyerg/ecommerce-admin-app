c74f8a79934d22209f305589b82df3a7e128331a
sonyerg <ericksonrguinto@gmail.com>
2024-05-20 20:42:53 +1200

    added form for store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useStoreModal } from "@/hooks/use-store-modal";
-
 import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function SetupPage() {
   const onOpen = useStoreModal((state) => state.onOpen);
